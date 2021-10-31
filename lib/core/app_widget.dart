488f6e02cf037f3789cb65124dfbcfe20b1225de
Matheus Schneider <matheus.ssm@gmail.com>
2021-10-31 17:51:55 -0300

    initial commit

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+import '../splash/splash_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/app_gradients.dart';
+import '../../../core/app_text_styles.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            decoration: BoxDecoration(
+              gradient: AppGradients.linear,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Olá, ",
+                    style: AppTextStyles.title,
+                    children: [
+                      TextSpan(
+                        text: "Matheus Schneider",
+                        style: AppTextStyles.titleBold,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/26752887?v=4",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+}

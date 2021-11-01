760572a55b295f1610a6b201d3f59defba20ffab
Matheus Schneider <matheus.ssm@gmail.com>
2021-11-01 16:41:53 -0300

    added challenge page, extracted progress indicator

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -36,14 +37,10 @@ class QuizCardWidget extends StatelessWidget {
               Expanded(
                   flex: 1, child: Text("3/10", style: AppTextStyles.body11)),
               Expanded(
-                flex: 4,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
-              ),
+                  flex: 4,
+                  child: ProgressIndicatorWidget(
+                    value: 0.3,
+                  )),
             ],
           ),
         ],

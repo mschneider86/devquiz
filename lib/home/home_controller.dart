7591eb51f8b38ca4fb55702e523c6559d3bf4e1b
Matheus Schneider <matheus.ssm@gmail.com>
2021-11-19 15:33:58 -0300

    add: home repository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -6,6 +6,8 @@ import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
+import 'home_repository.dart';
+
 class HomeController {
   final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
   set state(HomeState state) => stateNotifier.value = state;
@@ -14,69 +16,20 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
 
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: 'Matheus Schneider',
-      photoUrl: 'https://avatars.githubusercontent.com/u/26752887?v=4',
-    );
+    user = await repository.getUser();
 
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
 
-    quizzes = [
-      QuizModel(
-        title: 'Flutter Development',
-        image: AppImages.blocks,
-        questionsAnswered: 1,
-        level: Level.easy,
-        questions: [
-          QuestionModel(
-            title: 'Enjoying Flutter?',
-            answers: [
-              AnswerModel(
-                title: 'Yes, a little',
-              ),
-              AnswerModel(
-                title: 'Yes, a lot',
-              ),
-              AnswerModel(
-                title: 'Obviously',
-                isRight: true,
-              ),
-              AnswerModel(
-                title: 'Not at all',
-              ),
-            ],
-          ),
-          QuestionModel(
-            title: 'Enjoying Flutter?',
-            answers: [
-              AnswerModel(
-                title: 'Yes, a little',
-              ),
-              AnswerModel(
-                title: 'Yes, a lot',
-              ),
-              AnswerModel(
-                title: 'Obviously',
-                isRight: true,
-              ),
-              AnswerModel(
-                title: 'Not at all',
-              ),
-            ],
-          ),
-        ],
-      ),
-    ];
+    quizzes = await repository.getQuizzes();
 
     state = HomeState.success;
   }
